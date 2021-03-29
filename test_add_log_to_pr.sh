02fd1885ea147d682f06dcd67dfe0c39de167e6d
Dries Verachtert <dries.verachtert@dries.eu>
2021-03-29 21:39:08 +0200

    NOTICKET test again

--- test_add_log_to_pr.sh
+++ test_add_log_to_pr.sh
@@ -21,14 +21,25 @@ JOB_ID_OF_JOB1=$(curl --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" "https:/
 echo JOB_ID_OF_JOB1=${JOB_ID_OF_JOB1}
 
 # Get the log of job1.
-curl --location --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" "https://gitlab.example.com/api/v4/projects/${CI_PROJECT_ID}/jobs/${JOB_ID_OF_JOB1}/trace" > log1.txt
+curl --location --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" "https://bbpgitlab.epfl.ch/api/v4/projects/${CI_PROJECT_ID}/jobs/${JOB_ID_OF_JOB1}/trace" > log1.txt
 
 echo "==============" Start of cat of log1.txt =============="
 cat log1.txt
 echo "==============" End of cat of log1.txt =============="
 
+BODY_PART_1='{"public":false,"files":{"job1.txt":{'
+BODY_LOG_CONTENTS=$( jo contents="$(cat log1.txt)" )
+BODY_PART_2='}}}'
 
-curl -s -u 'driesverachtert:${GITHUB_API_KEY}' -H "Accept: application/vnd.github.groot-preview+json"  https://api.github.com/repos/BlueBrain/test_github_gitlab_integration/commits/${LATEST_COMMIT_SHA}/pulls | jq -r '.[]["_links"]["comments"]["href"]'
 
+
+curl -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/gists -d "${BODY_PART_1}${BODY_LOG_CONTENTS}${BODY_PART_2}"
+
+# for debugging, get all data about the PR that contains this commit
+curl -s -u 'driesverachtert:${GITHUB_API_KEY}' -H "Accept: application/vnd.github.groot-preview+json"  https://api.github.com/repos/BlueBrain/test_github_gitlab_integration/commits/${LATEST_COMMIT_SHA}/pulls 
+# just get the comments_url of this PR, so that we can add another comment
+COMMENTS_URL=$(curl -s -u 'driesverachtert:${GITHUB_API_KEY}' -H "Accept: application/vnd.github.groot-preview+json"  https://api.github.com/repos/BlueBrain/test_github_gitlab_integration/commits/${LATEST_COMMIT_SHA}/pulls | jq -r '.[]["_links"]["comments"]["href"]')
+
+echo COMMENTS_URL=${COMMENTS_URL}
 
 # curl -s -u 'driesverachtert:${GITHUB_API_KEY}' -X POST -d '{"body": "test message"}' https://api.github.com/repos/BlueBrain/test_github_gitlab_integration/issues/3/comments
